8407b5b537961d4a8c991ee19731be17271970cc
Alejandro Hernandez Valle <alejandrohernandezvalle@gmail.com>
2019-05-20 11:16:56 -0500

    update fix modal

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="es"> 
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,7 +14,7 @@
   <link href="{{asset('vendors/pace-progress/css/pace.min.css')}}" rel="stylesheet">
         
   <!-- Font Awesome-->
-  <link href="{{asset('css/bootstrap/all.css')}}" rel="stylesheet">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
     
   <!-- Theme style--> 
   <link rel="stylesheet" href="{{asset('css/AdminLTE.min.css')}}">
@@ -187,21 +187,20 @@
   </div>
 </footer>
   <!----------------------------------------------------------------------------------------------->
-  <!-- jQuery 2.1.4 -->
-  <script src="{{asset('js/jQuery-2.1.4.min.js')}}"></script>
-  @stack('scripts')
-
   <!-- Bootstrap 3.3.5-->
   <script src="{{asset('js/bootstrap.min.js')}}"></script>
   <!--AdminLTE App-->
   <script src="{{asset('js/app.min.js')}}"></script>
       
   <!--Bootsap 4.3-->
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <script src="{{asset('js/jquery-3.3.1.slim-cnd.min.js')}}"></script>
+  <script src="{{asset('js/popper-cnd.min.js')}}"></script>
+  <script src="{{asset('js/bootstrap-cnd.min.js')}}"></script>
 
   <!--Bootsap Select-->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.10/dist/js/bootstrap-select.min.js"></script>
+  <script src="{{asset('js/bootstrap-select-2.min.js')}}"></script>
+  <!--Jquery 3.4-->
+  <script src="{{asset('js/jquery-3.4.1.min.js')}}"></script>
+  @stack('scripts')
   </body>
 </html>
