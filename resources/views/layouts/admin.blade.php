34c6330b92c5b60a1ad793431f5566b8cce71dc6
Alejandro Hernandez Valle <alejandrohernandezvalle@gmail.com>
2019-05-20 15:29:53 -0500

    update de menu app, nav, dropdown

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -36,171 +36,175 @@
   <link rel="stylesheet" href="{{asset('css/stylehome.css')}}">
 <!---------------------------------------------------------------------------------------------->
 </head>
-<body class="app header-fixed sidebar-fixed aside-menu-fixed sidebar-lg-show">        
-<header class="app-header navbar">
-  <button class="navbar-toggler sidebar-toggler d-lg-none mr-auto" type="button" data-toggle="sidebar-show">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <a class="navbar-brand" href="#">
-    <img class="navbar-brand-full" src="{{asset('img/brand/logo.svg')}}" width="100" height="95" alt="Xixha Logo">
-    <img class="navbar-brand-minimized" src="{{asset('img/brand/sygnet.svg')}}" width="30" height="30" alt="CoreUI Logo">
-  </a>
-  <ul class="nav navbar-nav d-md-down-none">
-    <li class="nav-item px-3">
-      <a class="nav-link" href="{{asset('home')}}">Dashboard</a>
-    </li>
-  </ul>
-    <ul class="nav navbar-nav ml-auto">
-      <li class="nav-item d-md-down-none">
-        <a class="nav-link" href="#">
-          <i class="icon-list"></i>
-        </a>
+<body class="app header-fixed sidebar-fixed aside-menu-fixed sidebar-lg-show col-lg-12 col-md-12 col-sm-12 col-xs-12"> container">       
+  <header class="app-header navbar">
+
+    <!-- Buton de menu logo de perfil-->
+    <button class="navbar-toggler sidebar-toggler d-lg-none mr-auto" type="button" data-toggle="sidebar-show">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <a class="navbar-brand" href="#">
+      <img class="navbar-brand-full" src="{{asset('img/brand/logo.svg')}}" width="100" height="95" alt="Xixha Logo">
+      <img class="navbar-brand-minimized" src="{{asset('img/brand/sygnet.svg')}}" width="30" height="30" alt="Xixha Logo">
+    </a>
+    <ul class="nav navbar-nav d-md-down-none">
+      <li class="nav-item px-3">
+        <a class="nav-link" href="{{asset('home')}}">Dashboard</a>
       </li>
-      <li class="nav-item d-md-down-none">
-        <a class="nav-link" href="#">
-          <i class="icon-location-pin"></i>
-        </a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
-          <img class="img-avatar" src="{{asset('img/avatars/7.jpg')}}" alt="alejandrohernandezvalle@gmail.com">
-        </a>
-        <div class="dropdown-menu dropdown-menu-right">
-        <div class="dropdown-header text-center">
-          <strong>Cuenta</strong>
-        </div>
-        <a class="dropdown-item" href="#">
-          <i class="fa fa-user"></i> Perfil</a>
-        <a class="dropdown-item" href="#">
-          <i class="fa fa-wrench"></i> Configuraciones</a>
-          <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="{{url('/logout')}}">
-              <i class="fa fa-lock"></i> Salir</a>
-          </div>
-        </li>
-      </ul>  
-      <button class="navbar-toggler aside-menu-toggler d-lg-none" type="button" data-toggle="aside-menu-show">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-</header>  
-<div class="app-body">
-  <div class="sidebar">
-    <div class="sidebar-nav">
-      <div class="nav">
-        <li class="nav-item">
+    </ul>
+      <ul class="nav navbar-nav ml-auto">
+        <li class="nav-item d-md-down-none">
           <a class="nav-link" href="#">
-            <i class="nav-icon icon-speedometer"></i> Dashboard
-              <span class="badge badge-primary">Xixhá</span>
-            </a>
-          </li>
-        <div class="pos-f-t contrainer-fluid">
-        <div class="collapse" id="navbarToggleExternalContent">
-        <div class="bg-dark p-4">
-          <li class="nav-title">Almacé</li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{url('/almacen/articulo')}}">
-              <i class="nav-icon icon-drop"></i> Artículos
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{url('/almacen/categoria')}}">
-              <i class="nav-icon icon-drop"></i> Categorías
-            </a>
-          </li>
-          <li class="nav-title">Compras</li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{url('/compras/ingreso')}}">
-              <i class="nav-icon icon-drop"></i> Ingreso
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{url('/compras/proveedor')}}">
-              <i class="nav-icon icon-drop"></i> Proveedor
-            </a>
-          </li>
-
-          <li class="nav-title">Vetas</li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{url('/ventas/venta')}}">
-              <i class="nav-icon icon-drop"></i> Venta
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{url('/ventas/cliente')}}">
-              <i class="nav-icon icon-drop"></i> Cliente
-            </a>
-          </li>
-
-          <li class="nav-title">Acceso</li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{url('seguridad/usuario')}}">
-              <i class="nav-icon icon-drop"></i> Usuarios
-            </a>
-          </li>
-        </div>
-      </div>
-      <nav class="navbar navbar-dark bg-dark">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-      </nav>
-    </div>
-        </div>
-      </div>
-    </div>
-  </div>
-<div class="content-wrapper">
-  <section class="content">
-    <div class="row">
-        <div class = "col-lg-12 col-sm-12 col-md-12 col-xs-12">
-        <div class="box">
-          <div class="box-header with-border">
-            <h3 class="box-title">XIXHÁ</h3>
-            <div class="box-tools pull-right">
-              <button class="btn btn-box-tool" data-widget="collapse">
-                <i class="fa fa-minus"></i>
-              </button>
-              <button class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+            <i class="icon-list"></i>
+          </a>
+        </li>
+        <li class="nav-item d-md-down-none">
+          <a class="nav-link" href="#">
+            <i class="icon-location-pin"></i>
+          </a>
+        </li>
+        <!-- Etiqueta de activacion de boton logo perfil-->
+        <li class="nav-item dropdown">
+          <a class="nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+            <img class="img-avatar" src="{{asset('img/avatars/7.jpg')}}" alt="alejandrohernandezvalle@gmail.com">
+          </a>
+          <div class="dropdown-menu dropdown-menu-right">
+          <div class="dropdown-header text-center">
+            <strong>Cuenta</strong>
+          </div>
+          <a class="dropdown-item" href="#">
+            <i class="fa fa-user"></i> Perfil</a>
+          <a class="dropdown-item" href="#">
+            <i class="fa fa-wrench"></i> Configuraciones</a>
+            <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="{{url('/logout')}}">
+                <i class="fa fa-lock"></i> Salir
+              </a>
             </div>
-            <div class="box-body">
-              <div class="row">
-                <div class = "col-lg-12 col-sm-12 col-md-12 col-xs-12">
-                  @yield('contenido')
+          </li>
+        </ul> 
+  </header>  
+  <div class="app-body">
+    <div class="sidebar">
+      <div class="sidebar-nav">
+
+        <div class="nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <i class="nav-icon icon-speedometer"></i> Dashboard
+                <span class="badge badge-primary">Xixhá</span>
+              </a>
+            </li>
+          <div class="pos-f-t contrainer-fluid">
+          <div class="collapse" id="navbarToggleExternalContent">
+          <div class="bg-dark p-4">
+            <li class="nav-title">Almacé</li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{url('/almacen/articulo')}}">
+                <i class="nav-icon icon-drop"></i> Artículos
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{url('/almacen/categoria')}}">
+                <i class="nav-icon icon-drop"></i> Categorías
+              </a>
+            </li>
+            <li class="nav-title">Compras</li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{url('/compras/ingreso')}}">
+                <i class="nav-icon icon-drop"></i> Ingreso
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{url('/compras/proveedor')}}">
+                <i class="nav-icon icon-drop"></i> Proveedor
+              </a>
+            </li>
+
+            <li class="nav-title">Vetas</li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{url('/ventas/venta')}}">
+                <i class="nav-icon icon-drop"></i> Venta
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{url('/ventas/cliente')}}">
+                <i class="nav-icon icon-drop"></i> Cliente
+              </a>
+            </li>
+
+            <li class="nav-title">Acceso</li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{url('seguridad/usuario')}}">
+                <i class="nav-icon icon-drop"></i> Usuarios
+              </a>
+            </li>
+          </div>
+        </div>
+        <nav class="navbar navbar-dark bg-dark">
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </nav>
+      </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  <div class="content-wrapper">
+    <section class="content">
+      <div class="row">
+          <div class = "col-lg-12 col-sm-12 col-md-12 col-xs-12">
+          <div class="box">
+            <div class="box-header with-border">
+              <h3 class="box-title">XIXHÁ</h3>
+              <div class="box-tools pull-right">
+                <button class="btn btn-box-tool" data-widget="collapse">
+                  <i class="fa fa-minus"></i>
+                </button>
+                <button class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+              </div>
+              <div class="box-body">
+                <div class="row">
+                  <div class = "col-lg-12 col-sm-12 col-md-12 col-xs-12">
+                    @yield('contenido')
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </section>
-</div>
-<!------------------------------------------------------------------------------------------------------------->
-<footer class="app-footer">
-  <div>
-    <a href="#">Xixhá</a>
-    <span>&copy; 2019 Xixhá.</span>
+    </section>
   </div>
-  <div class="ml-auto">
-    <span>Powered by</span>
+  <!------------------------------------------------------------------------------------------------------------->
+  <footer class="app-footer">
+    <div>
       <a href="#">Xixhá</a>
-  </div>
-</footer>
-  <!----------------------------------------------------------------------------------------------->
-  <!-- Bootstrap 3.3.5-->
-  <script src="{{asset('js/bootstrap.min.js')}}"></script>
-  <!--AdminLTE App-->
-  <script src="{{asset('js/app.min.js')}}"></script>
-      
-  <!--Bootsap 4.3-->
-  <script src="{{asset('js/jquery-3.3.1.slim-cnd.min.js')}}"></script>
-  <script src="{{asset('js/popper-cnd.min.js')}}"></script>
-  <script src="{{asset('js/bootstrap-cnd.min.js')}}"></script>
+      <span>&copy; 2019 Xixhá.</span>
+    </div>
+    <div class="ml-auto">
+      <span>Powered by</span>
+        <a href="#">Xixhá</a>
+    </div>
+  </footer>
+    <!----------------------------------------------------------------------------------------------->
 
-  <!--Bootsap Select-->
-  <script src="{{asset('js/bootstrap-select-2.min.js')}}"></script>
-  <!--Jquery 3.4-->
-  <script src="{{asset('js/jquery-3.4.1.min.js')}}"></script>
-  @stack('scripts')
+    <!--Bootsap 4.3-->
+    <script src="{{asset('js/jquery-3.3.1.slim-cnd.min.js')}}"></script>
+    <script src="{{asset('js/popper-cnd.min.js')}}"></script>
+    <script src="{{asset('js/bootstrap-cnd.min.js')}}"></script>
+
+    <!--Bootsap Select-->
+    <script src="{{asset('js/bootstrap-select-2.min.js')}}"></script>
+    <!--Jquery 3.4-->
+    <script src="{{asset('js/jquery-3.4.1.min.js')}}"></script>
+    @stack('scripts')
+    
+    <!-- Bootstrap 3.3.5-->
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <!--AdminLTE App-->
+    <script src="{{asset('js/app.min.js')}}"></script>
   </body>
 </html>
