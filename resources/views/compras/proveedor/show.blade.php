1a133bf1cf37f88bd9c86467e7de1202ad2ee76c
Alejandro Hernandez Valle <alejandrohernandezvalle@gmail.com>
2019-07-12 15:21:36 -0500

    update e-estilo

--- resources/views/compras/proveedor/show.blade.php
+++ resources/views/compras/proveedor/show.blade.php
@@ -1,7 +1,7 @@
 @extends ('layouts.admin')
 @section ('contenido')
 	<div class = "row"> 
-		<div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
+		<div class="col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto">
 			<h3>Datos del Apicultor: {{$persona->nombre}}</h3>
 			@if(count($errors)>0)
 				<div class="alert alert-danger">
@@ -17,180 +17,181 @@
 	{!!Form::model($persona,['method'=>'PATCH','route'=>['compras.proveedor.update',$persona->idpersona]]) !!}
 	{{Form::token()}}
 	<div class="row">
-		<div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
-			<h3 class="text-center">Datos Personales del Apicultor</h3><br>
-		</div>
-		
-		<div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
-			<div class="form-group mf-form amber-textarea active-amber-textarea-2 input-group mb-3">
-				<div class="input-group-append">
-					<span class="input-group-text btn btn-warning" id="basic-addon2">Folio de: {{$persona->nombre}} {{$persona->apellidopa}} {{$persona->apellidoma}}</span><br>
-				</div>
-				<input type="text" name="folio" value="{{$persona->folio}}" disabled ="true" class="md-textarea form-control lineA " aria-describedby="basic-addon2">
+		<div class="container">
+
+			<!---Titulo---->
+			<div class="col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto">
+				<h3 class="text-center e-text-color">Datos Personales del Apicultor</h3>
 			</div>
-		</div>
+
+			<!---Nombre del apicultor---->			
+			<br><br>
+			<div class="col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto e-border"> 
+				<div class="form-group mf-form amber-textarea active-amber-textarea-2 input-group mb-3 e-border text-center">
+					<div class="input-group-append mx-auto">
+						<span class="input-group-text btn btn-warning col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto" id="basic-addon101">Folio de: {{$persona->nombre}} {{$persona->apellidopa}} {{$persona->apellidoma}}</span>
+					</div><br>
+					<input type="text" name="folio" value="{{$persona->folio}}" disabled ="true" class="e-border md-textarea form-control lineA" aria-describedby="basic-addon101">
+				</div>
+			</div><br><br>
+				
+			<!---Estado---->			
+			<div class="col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto e-border">		
+				<div class="input-group-append">
+					<span class="input-group-text btn btn-warning col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto" id="basic-addon102">Estado</span>
+					<input type="text" name="estadoP" value="{{$persona->estadoP}}" disabled ="true" class="e-border md-textarea form-control lineA" aria-describedby="basic-addon102">			
+				</div>
+			</div><br><br>
 			
-		<div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
-			<div class="form-group mf-form amber-textarea active-amber-textarea-2 input-group mb-3">
-				<input type="text" name="estadoP" value="{{$persona->estadoP}}" disabled ="true" class="md-textarea form-control lineA " aria-describedby="basic-addon2">
+			<!--Municipio-->
+			<div class="col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto e-border">			
 				<div class="input-group-append">
-					<span class="input-group-text btn btn-warning" id="basic-addon2">Estado</span><br>
+					<span class="input-group-text btn btn-warning col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto" id="basic-addon103">Municipio</span>				
+					<input type="text" name="municipio" value="{{$persona->municipio}}" disabled ="true" class="e-border md-textarea form-control lineA" aria-describedby="basic-addon103">
 				</div>
+			</div><br><br>
 
-				<input type="text" name="municipio" value="{{$persona->municipio}}" disabled ="true" class="md-textarea form-control lineA " aria-describedby="basic-addon2">
+			<!--Colonia-->
+			<div class="col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto e-border">
 				<div class="input-group-append">
-					<span class="input-group-text btn btn-warning" id="basic-addon2">Municipio</span><br>
+					<span class="input-group-text btn btn-warning col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto" id="basic-addon104">Colonia</span><br>
+					<input type="text" name="colonia" value="{{$persona->colonia}}" disabled ="true" class="e-border md-textarea form-control lineA" aria-describedby="basic-addon104">
 				</div>
+			</div><br><br>
 
-				<input type="text" name="colonia" value="{{$persona->colonia}}" disabled ="true" class="md-textarea form-control lineA " aria-describedby="basic-addon2">
+			<!--Calle-->
+			<div class="col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto e-border">
 				<div class="input-group-append">
-					<span class="input-group-text btn btn-warning" id="basic-addon2">Colonia</span><br>
+					<span class="input-group-text btn btn-warning col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto" id="basic-addon105">Calle</span><br>
+					<input type="text" name="calle" value="{{$persona->calle}}" disabled ="true" class="e-border md-textarea form-control lineA" aria-describedby="basic-addon105">
 				</div>
+			</div><br><br>
 
-				<input type="text" name="calle" value="{{$persona->calle}}" disabled ="true" class="md-textarea form-control lineA " aria-describedby="basic-addon2">
+			<!--Telefono-->
+			<div class="col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto e-border">
 				<div class="input-group-append">
-					<span class="input-group-text btn btn-warning" id="basic-addon2">Calle</span><br>
+					<span class="input-group-text btn btn-warning col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto" id="basic-addon106">Telefono</span><br>
+					<input type="text" name="telefono" value="{{$persona->telefono}}" disabled ="true" class="e-border md-textarea form-control lineA" aria-describedby="basic-addon106">
 				</div>
-			</div>			
-		</div>
+			</div><br><br>
 
-		<div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
-			<div class="form-group mf-form amber-textarea active-amber-textarea-2 input-group mb-3">
-				<input type="text" name="telefono" value="{{$persona->telefono}}" disabled ="true" class="md-textarea form-control lineA " aria-describedby="basic-addon2">
+			<!--Email-->
+			<div class="col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto e-border">
 				<div class="input-group-append">
-					<span class="input-group-text btn btn-warning" id="basic-addon2">Telefono</span><br>
+					<span class="input-group-text btn btn-warning col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto" id="basic-addon107">Correo electronico</span><br>
+					<input type="text" name="email" value="{{$persona->email}}" disabled ="true" class="e-border md-textarea form-control lineA" aria-describedby="basic-addon107">
 				</div>
-			</div>
-		</div>
+			</div><br><br>
 
-		<div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
-			<div class="form-group mf-form amber-textarea active-amber-textarea-2 input-group mb-3">
-				<input type="text" name="email" value="{{$persona->email}}" disabled ="true" class="md-textarea form-control lineA  " aria-describedby="basic-addon2">
-					<div class="input-group-append">
-						<span class="input-group-text btn btn-warning" id="basic-addon2">Correo electronico</span><br>
-					</div>
-			</div>
-		</div>
+			<!--Sexo-->
+			<div class="col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto e-border">
+				<div class="input-group-append ">
+					<span class="input-group-text btn btn-warning col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto" id="basic-addon108">SEXO</span><br>				
+					<input type="text" name="sexo" value="{{$persona->sexo}}" disabled ="true" class="e-border md-textarea form-control lineA" aria-describedby="basic-addon108">
+				</div>
+			</div><br><br>
 
-		<div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
-			<div class="form-group mf-form amber-textarea active-amber-textarea-2 input-group mb-3">
-				<input type="text" name="sexo" value="{{$persona->sexo}}" disabled ="true" class="md-textarea form-control lineA  " aria-describedby="basic-addon2">
+			<!--CURP-->			
+			<div class="col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto e-border">
 				<div class="input-group-append">
-					<span class="input-group-text btn btn-warning" id="basic-addon2">SEXO</span><br>
-				</div>
-			</div>
-		</div>
-
-		<div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
-			<div class="form-group mf-form amber-textarea active-amber-textarea-2 input-group mb-3">
-				<input type="text" name="curp" value="{{$persona->curp}}" disabled ="true" class="md-textarea form-control lineA " aria-describedby="basic-addon2">
-					<div class="input-group-append">
-						<span class="input-group-text btn btn-warning" id="basic-addon2">CURP</span><br>
-					</div>
-			</div>
-		</div>
-
-		<div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
-			<div class="form-group mf-form amber-textarea active-amber-textarea-2 input-group mb-3">
-				<input type="text" name="rfc" value="{{$persona->rfc}}" disabled ="true" class="md-textarea form-control lineA " aria-describedby="basic-addon2">
-					<div class="input-group-append">
-						<span class="input-group-text btn btn-warning" id="basic-addon2">RFC</span><br>
-					</div>
-			</div>
-		</div>
-	
-		<div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
-			<br><h3 class="text-center">Datos de Rastreabilidad</h3><br>
-		</div>
-
-		<div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
-			<div class="form-group mf-form amber-textarea active-amber-textarea-2 input-group mb-3">
-				<div class="input-group-append">
-					<span class="input-group-text btn btn-warning" id="basic-addon2">Folio</span><br>
-				</div>
-				<input type="text" name="folio" value="{{$persona->folio}}" disabled ="true" class="md-textarea form-control lineA " aria-describedby="basic-addon2">
-			</div>
-		</div>
-
-		<div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
-			<div class="form-group mf-form amber-textarea active-amber-textarea-2 input-group mb-3">
-				<input type="text" name="certificacion" value="{{$persona->certificacion}}" disabled ="true" class="md-textarea form-control lineA " aria-describedby="basic-addon2">
-				<div class="input-group-append">
-					<span class="input-group-text btn btn-warning" id="basic-addon2">Tipo de Certificación</span><br>
-				</div>
-			</div>
-		</div>
-
-		<div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
-			<div class="form-group mf-form amber-textarea active-amber-textarea-2 input-group mb-3">
-				<input type="text" name="temp_cosecha" value="{{$persona->temp_cosecha}}" disabled ="true" class="md-textarea form-control lineA " aria-describedby="basic-addon2">
-				<div class="input-group-append">
-					<span class="input-group-text btn btn-warning" id="basic-addon2">Temporada de Cosecha</span><br>
-				</div>
-			</div>
-		</div> 
-
-		<div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
-			<div class="form-group mf-form amber-textarea active-amber-textarea-2 input-group mb-3">
-				<div class="input-group-append">
-					<span class="input-group-text btn btn-warning" id="basic-addon2">Numeros de Colmenas o Chapetas</span>
-				</div>
-				<input type="text" name="num_colmena" value="{{$persona->num_colmena}}" disabled ="true" class="md-textarea form-control lineA " aria-describedby="basic-addon2">
-			</div>
-		</div>
-	
-		<div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
-			<div class="form-group mf-form amber-textarea active-amber-textarea-2 input-group mb-3">
-				<div class="input-group-append">
-					<span class="input-group-text btn btn-warning" id="basic-addon2">UPP</span><br>
-				</div>
-				<input type="text" name="upp" value="{{$persona->upp}}" disabled ="true" class="md-textarea form-control lineA " aria-describedby="basic-addon2">
-			</div>
-		</div> 
-		
-		<div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
-			<div class="form-group mf-form amber-textarea active-amber-textarea-2 input-group mb-3">
-			<input type="text" name="pgn" value="{{$persona->pgn}}" disabled ="true" class="md-textarea form-control lineA " aria-describedby="basic-addon2">
-				<div class="input-group-append">
-					<span class="input-group-text btn btn-warning" id="basic-addon2">PGN</span><br>
-				</div>
-			</div>
-		</div>
-	
-		<div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
-			<div class="form-group mf-form amber-textarea active-amber-textarea-2 input-group mb-3">
-				<input type="text" name="prod_anual" value="{{$persona->prod_anual}}" disabled ="true" class="md-textarea form-control lineA " aria-describedby="basic-addon2">
-				<div class="input-group-append">
-					<span class="input-group-text btn btn-warning" id="basic-addon2">Produccion anual</span><br>
-				</div>
-			</div>
-		</div>  
-		
-		<div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
-			<div class="form-group mf-form amber-textarea active-amber-textarea-2 input-group mb-3">
-				<div class="input-group-append">
-					<span class="input-group-text btn btn-warning" id="basic-addon2">Clave de Rasteabilidad</span><br>
-				</div>
-				<input type="text" name="clave_rast" value="{{$persona->clave_rast}}" disabled ="true" class="md-textarea form-control lineA " aria-describedby="basic-addon2">
-			</div>
-		</div>
-
-		<div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
-			<div class="form-group mf-form amber-textarea active-amber-textarea-2 input-group mb-3">
-				<div class="input-group-append">
-					<span class="input-group-text btn btn-warning" id="basic-addto2">Localización de los Apiarios</span><br>
+					<span class="input-group-text btn btn-warning col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto" id="basic-addon110">CURP</span><br>		
+					<input type="text" name="curp" value="{{$persona->curp}}" disabled ="true" class="e-border md-textarea form-control lineA" aria-describedby="basic-addon109">		
 				</div>	
-				<input type="text" name="loc_api" value="{{$persona->loc_api}}" disabled ="true" class="md-textarea form-control lineA " aria-describedby="basic-addto2">
-			</div>
-		</div>
+			</div><br><br>
 
-		<div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
-			<div class="form-group mf-form amber-textarea active-amber-textarea-2 input-group mb-3">
+			<!--RFC-->
+			<div class="col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto e-border">
 				<div class="input-group-append">
-					<span class="input-group-text" id="basic-addon2">Observación</span><br>
+					<span class="input-group-text btn btn-warning col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto" id="basic-addon2">RFC</span><br>					
+					<input type="text" name="rfc" value="{{$persona->rfc}}" disabled ="true" class="e-border md-textarea form-control lineA" aria-describedby="basic-addon110">
 				</div>
-				<input type="text" name="observacion" value="{{$persona->observacion}}" disabled ="true" class="md-textarea form-control lineA " aria-describedby="basic-addon2">
+			</div><br><br>
+		
+			<!--Titulo de Rastreabilidad-->
+			<div class="col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto e-border">
+				<br><h3 class="text-center">Datos de Rastreabilidad</h3><br>
 			</div>
+
+			<!--Folio-->
+			<div class="col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto e-border">				
+				<div class="input-group-append">
+					<span class="input-group-text btn btn-warning col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto" id="basic-addon111">Folio</span><br>
+					<input type="text" name="folio" value="{{$persona->folio}}" disabled ="true" class="e-border md-textarea form-control lineA " aria-describedby="basic-addon111">
+				</div>
+			</div><br><br>
+			
+			<!--Tipo-->
+			<div class="col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto e-border">	
+				<div class="input-group-append">
+					<span class="input-group-text btn btn-warning col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto" id="basic-addon112">Tipo de Certificación</span><br>	
+					<input type="text" name="certificacion" value="{{$persona->certificacion}}" disabled ="true" class="e-border md-textarea form-control lineA " aria-describedby="basic-addon112">
+				</div>
+			</div><br><br>
+
+			<!--Temporada-->
+			<div class="col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto e-border">	
+				<div class="input-group-append">
+					<span class="input-group-text btn btn-warning col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto" id="basic-addon113">Temporada de Cosecha</span><br>
+					<input type="text" name="temp_cosecha" value="{{$persona->temp_cosecha}}" disabled ="true" class="e-border md-textarea form-control lineA " aria-describedby="basic-addon113">
+				</div>
+			</div><br><br>
+
+			<!--N°-->
+			<div class="col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto e-border">				
+				<div class="input-group-append">
+					<span class="input-group-text btn btn-warning col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto" id="basic-addon114">N° Colmenas, Chapetas</span>
+					<input type="text" name="num_colmena" value="{{$persona->num_colmena}}" disabled ="true" class="e-border md-textarea form-control lineA " aria-describedby="basic-addon114">
+				</div>
+			</div><br><br>
+		
+			<!--UPP-->
+			<div class="col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto e-border">				
+				<div class="input-group-append">
+					<span class="input-group-text btn btn-warning col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto" id="basic-addon115">UPP</span><br>
+					<input type="text" name="upp" value="{{$persona->upp}}" disabled ="true" class="e-border md-textarea form-control lineA " aria-describedby="basic-addon115">			
+				</div>
+			</div><br><br>
+			
+			<!--PGN-->
+			<div class="col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto e-border">	
+				<div class="input-group-append">
+					<span class="input-group-text btn btn-warning col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto" id="basic-addon116">PGN</span><br>
+					<input type="text" name="pgn" value="{{$persona->pgn}}" disabled ="true" class="e-border md-textarea form-control lineA " aria-describedby="basic-addon116">							
+				</div>
+			</div><br><br>
+		
+			<!--Producción-->
+			<div class="col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto e-border">		
+				<div class="input-group-append">
+					<span class="input-group-text btn btn-warning col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto" id="basic-addon117">Producción anual</span><br>
+					<input type="text" name="prod_anual" value="{{$persona->prod_anual}}" disabled ="true" class="e-border md-textarea form-control lineA " aria-describedby="basic-addon117">
+				</div>
+			</div><br><br>
+			
+			<!--Clave-->
+			<div class="col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto e-border">				
+				<div class="input-group-append">
+					<span class="input-group-text btn btn-warning col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto" id="basic-addon118">Clave de Rasteabilidad</span><br>
+					<input type="text" name="clave_rast" value="{{$persona->clave_rast}}" disabled ="true" class="e-border md-textarea form-control lineA " aria-describedby="basic-addon118">
+				</div>
+			</div><br><br>
+
+			<!--Localización-->
+			<div class="col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto e-border">				
+				<div class="input-group-append">
+					<span class="input-group-text btn btn-warning col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto" id="basic-addto119">Localización de los Apiarios</span><br>
+					<input type="text" name="loc_api" value="{{$persona->loc_api}}" disabled ="true" class="e-border md-textarea form-control lineA " aria-describedby="basic-addto119">
+				</div>
+			</div><br><br>
+
+			<!--Observación-->
+			<div class="col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto e-border">				
+				<div class="input-group-append ">
+					<span class="input-group-text col-lg-9 col-sm-9 col-md-9 col-xs-9 mx-auto" id="basic-addon120">Observación</span><br>
+					<input type="text" name="observacion" value="{{$persona->observacion}}" disabled ="true" class="e-border md-textarea form-control lineA " aria-describedby="basic-addon120">
+				</div>
+			</div><br><br>
+
 		</div>
 	</div>
 	{!!Form::Close()!!}		
