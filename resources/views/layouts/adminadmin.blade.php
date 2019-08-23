018552ba0a864b7019604fe67090b67104e8f3f7
Alejandro Hernandez Valle <alejandrohernandezvalle@gmail.com>
2019-08-23 09:49:29 -0500

    update register 23082019

--- resources/views/layouts/adminadmin.blade.php
+++ resources/views/layouts/adminadmin.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="es"> 
-<head>
-  <meta charset="utf-8">  
+<head> 
+  <meta charset="utf-8"> 
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>SIPAX</title>   
@@ -14,24 +14,24 @@
 </head>
 <body>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a href="{{asset('/')}}"><img class="navbar-brand-minimized" src="{{asset('img/brand/sygnet.svg')}}" width="90" height="90" alt="Xixha Logo"></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle e-text-color" href="" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">SIPAX</a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <a class="dropdown-item btn btn-primary btn-responsive text-center e-text-color-y" href="{{url('administradors')}}">Inicio</a>
-            <a class="dropdown-item btn btn-primary btn-responsive text-center e-text-color-y" href="{{url('administradors/padron')}}">Padrón Apicultores</a>
-            <a class="dropdown-item btn btn-success btn-responsive text-center e-text-color-b" href="{{url('administradors/create')}}">Registro Apicultor</a>
-            <a class="dropdown-item btn btn-primary btn-responsive text-center e-text-color-r"  href="{{url('/logout')}}">Salir</a>
-          </div>
-        </li>
-      </ul>
-    </div>
-  </nav>
+    <a href="{{url('/')}}"><img class="navbar-brand-minimized" src="{{asset('img/brand/sygnet.svg')}}" width="90" height="90" alt="Xixha Logo"></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle e-text-color" href="" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">SIPAX</a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a class="dropdown-item btn btn-warning btn-responsive text-center te1" href="{{url('administradors')}}">Inicio</a>
+              <a class="dropdown-item btn btn-primary btn-responsive text-center te2" href="{{url('administradors/padron')}}">Padrón Apicultores</a>
+              <a class="dropdown-item btn btn-success btn-responsive text-center te2" href="{{url('administradors/create')}}">Registro Apicultor</a>
+              <a class="dropdown-item btn btn-danger btn-responsive text-center te3"  href="{{url('/logout')}}">Salir</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
   <div class="container-fluid"> 
     @yield('contenido')
   </div>
--- resources/views/layouts/adminuser.blade.php
+++ resources/views/layouts/adminuser.blade.php
@@ -23,9 +23,9 @@
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle e-text-color" href="" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">SIPAX</a>
           <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <a class="dropdown-item btn btn-primary btn-responsive text-center e-text-color-b" href="{{url('home')}}">Inicio</a>
-            <a class="dropdown-item btn btn-primary btn-responsive text-center e-text-color-b" href="{{url('padron')}}">Padrón</a>
-            <a class="dropdown-item btn btn-primary btn-responsive text-center e-text-color-r"  href="{{url('logout')}}">Salir</a>
+            <a class="dropdown-item btn btn-primary btn-responsive text-center" href="{{url('home')}}">Inicio</a>
+            <!--a class="dropdown-item btn btn-primary btn-responsive text-center e-text-color-b" href="{ {url('padron')}}">Padrón</a-->
+            <a class="dropdown-item btn btn-primary btn-responsive text-center"  href="{{url('logout')}}">Salir</a>
           </div>
         </li>
       </ul>
