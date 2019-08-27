2395fbf4195ae988a242c996aced43f30b5bbd85
Alejandro Hernandez Valle <alejandrohernandezvalle@gmail.com>
2019-08-27 10:20:30 -0500

    update 27082019

--- resources/views/supervisors/index.blade.php
+++ resources/views/supervisors/index.blade.php
@@ -45,7 +45,7 @@
         <main role="main">                
           <div class="jumbotron">
             <!--Titulo principal-->
-            <h1 class="text-center display-4 e-coloresx">Hola, Amigo Supervisor!</h1>
+            <h1 class="text-center display-4 e-coloresx e-element-11">Hola, Amigo Supervisor!</h1>
             <p class="text-center e-estilo-p">El sistema de padrón de apicultores Xixhá, tiene la finalidad de llevar la rastreabilidad de la miel de cada apicultor de cada municipio de la costa chica y montaña del estado del guerrero.</p>
             <p class="text-center"><a class="btn btn-primary btn-lg" href="{{url('supervisors/padron')}}" role="button">Ir a Padrón &raquo;</a></p>
           <div>
@@ -58,9 +58,9 @@
           <!--Card1-->
           <div class="col-md-4">
             <div class="card text-center">
-              <div class="card-body">
+              <div class="card-body e-element-2">
                 <h5 class="card-title text-primary">Mieles Xixhá</h5>
-                <p class="card-text text-justify">Las mieles Xixhá provienen de la región, Costa Chica de Guerrero, se caracterizan por ser mieles muy aromáticas, un poco obscuras, comparadas con las mielesdel altiplano, pero con aromas y sabores muy marcados, intensos o suaves, pero definidos que se han ganado la aceptaciónde los consumidores.</p>
+                <p class="card-text text-justify e-element-1">Las mieles Xixhá provienen de la región, Costa Chica de Guerrero, se caracterizan por ser mieles muy aromáticas, un poco obscuras, comparadas con las mielesdel altiplano, pero con aromas y sabores muy marcados, intensos o suaves, pero definidos que se han ganado la aceptaciónde los consumidores.</p>
                 <a class="btn btn-primary" href="http://xixha.com/wp-content/uploads/2019/06/Caracterizacion-de-la-Miel-Xixha.pdf" role="button">Detalles &raquo;</a>
               </div>
             </div>
@@ -69,9 +69,9 @@
           <!--Card2-->
           <div class="col-md-4">
             <div class="card text-center">          
-              <div class="card-body">            
+              <div class="card-body e-element-2">            
                 <h5 class="card-title text-primary">Proyecto Estratégico: “Bio Parque Xixhá”</h5>
-                <p class="card-text text-justify">Es un proyecto estratégico porque plantea una solución común a una problemática común, de bajo costo, alto impacto social, que genera empleo, arraigo comunitario, patrimonio familiar, forma agentes de cambio social guardianes de la naturaleza, al preservar los recursos de la selva y el bosque; tierra, agua, flora, fauna, oxigeno, combate al cambio climático..</p>
+                <p class="card-text text-justify e-element-1">Es un proyecto estratégico porque plantea una solución común a una problemática común, de bajo costo, alto impacto social, que genera empleo, arraigo comunitario, patrimonio familiar, forma agentes de cambio social guardianes de la naturaleza, al preservar los recursos de la selva y el bosque; tierra, agua, flora, fauna, oxigeno, combate al cambio climático..</p>
                 <a class="btn btn-primary" href="http://xixha.com/wp-content/uploads/2019/03/Bio-Parque-Xixha.pdf" role="button">Detalles &raquo;</a>
               </div>
             </div>
@@ -80,9 +80,9 @@
           <!--Card3-->
           <div class="col-md-4">
             <div class="card text-center">        
-              <div class="card-body">
+              <div class="card-body e-element-2">
                 <h5 class="card-title text-primary">Ficha Técnica Mieles Xixhá</h5>
-                <p class="card-text text-justify">Nuestras mieles tienen aplicación nutricional, pueden degustarse directamente, en mesa, en platillos en cocina, en panaderías, pastelerías, en confitería (caramelos, granola, etc.), en cosméticos (cremas, shampoo, etc.), en coadyuvantes para la salud, jarabes, elaboraciónde cervezas, etc.</p>
+                <p class="card-text text-justify e-element-1">Nuestras mieles tienen aplicación nutricional, pueden degustarse directamente, en mesa, en platillos en cocina, en panaderías, pastelerías, en confitería (caramelos, granola, etc.), en cosméticos (cremas, shampoo, etc.), en coadyuvantes para la salud, jarabes, elaboraciónde cervezas, etc.</p>
                 <a class="btn btn-primary" href="http://xixha.com/wp-content/uploads/2019/06/Ficha-TecnicaMiel-Xixha-100619.pdf" role="button">Detalles &raquo;</a>
               </div>
             </div>
@@ -92,7 +92,8 @@
 
       <!--Footer-->         
       <footer class="container-fliud">
-        <a href="http://xixha.com"><p class="e-coloresx e-footer">&copy;Xixhá 2019</p></a>
+        <a href="http://xixha.com"><p class="foorter-color-e">&copy;Xixhá 2019</p>
+        </a>
       </footer>      
 
     </div>
