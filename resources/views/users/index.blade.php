1f6c09da8c6aa8736ed26d706afb2dd2358d8dff
Alejandro Hernandez Valle <alejandrohernandezvalle@gmail.com>
2019-09-10 11:15:29 -0500

    update user

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -95,7 +95,7 @@
     </div>
   </div>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="{{asset('js/jsx/jquery-slim.min.js')}}"></script>
+  <!--script src="{ {asset('js/jsx/jquery-slim.min.js')}}"></script-->
   <script src="{{asset('js/jsx//bootstrap.bundle.min.js')}}"></script>
   </body>
 </html>
